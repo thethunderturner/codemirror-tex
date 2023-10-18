e9bdf13a73b27942309456bc2eec37dbe77de97e
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-10-18 15:00:36 +0800

    refactor: upgrade codemirror setup package

--- packages/lang-tex/src/autocomplete/mathjax/index.ts
+++ packages/lang-tex/src/autocomplete/mathjax/index.ts
@@ -1,8 +1,9 @@
 import { completeFromList } from '@codemirror/autocomplete';
 import { texLanguage } from '../../tex';
 import mathjaxSnippetDefinitions from './mathjax.snippet.json';
+import { Extension } from '@codemirror/state';
 
 // A collection of MathJax snippets for autocompletion.
-export const mathjaxCompletion = texLanguage.data.of({
+export const mathjaxCompletion : Extension = texLanguage.data.of({
   autocomplete: completeFromList(mathjaxSnippetDefinitions),
 });
